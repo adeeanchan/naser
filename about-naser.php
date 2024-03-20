d08a6646cd31ac7ed49e13b047c2c434e9ab6113
digitizercorp <digitizercorpuae@gmail.com>
2024-03-20 12:32:47 +0400

    updated-comingsoon-remove-naser

--- about-naser.php
+++ about-naser.php
@@ -1,13 +1,64 @@
 <?php
 include('header.php');
 ?>
+<header class="page-header" data-background="https://res.cloudinary.com/dcesrrt95/image/upload/v1694383637/valcom/nasser/naser-group/page_bfqnhz.jpg" data-stellar-background-ratio="1.15">
+	<div class="container">
+		<h1>About Naser</h1>
+		  <ol class="breadcrumb">
+    <li class="breadcrumb-item active" aria-current="page"><a href="#">About us</a></li>
+    <li class="breadcrumb-item " > <a href="#gallery">Gallery</a> </li>
+    <li class="breadcrumb-item "> <a href="#milestones">Milestones</a> </li>
+  </ol>
+	</div>
+	<!-- end container -->
+</header>
 <!-- end page-header -->
-<section >
-	<!-- <img src="" alt=""> -->
-  <div class="coming-soon chutiya">
-    <img src="https://res.cloudinary.com/adee-anchan/image/upload/v1710066610/valcom/27282_oyq7me.jpg" alt="">
-  </div>
+<section class="about-content">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				  <h2><span>Naser</span> Excellency</h2>
+        <h5>"Quality is not an act, it is a habit."</h5>
+			</div>
+			<!-- end col-12 -->
+			 <div class="col-12">
+        <p>For more than two decades, Naser Group has been dedicated to the pursuit of excellence in every project we embark upon. Our unwavering commitment to transforming spaces and setting new industry benchmarks infuses innovation and uncompromising quality into each construction and development venture
+          we undertake.</p>
+          
+      </div>
+      
+      <div class="col-12" id="gallery">
+        <div class="gallery-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide"><img src="https://res.cloudinary.com/adee-anchan/image/upload/v1696580099/valcom/nasser/naser-group/24_s64bci_apedrg.png" alt="Image"></div>
+      <div class="swiper-slide"><img src="https://res.cloudinary.com/adee-anchan/image/upload/v1696580094/valcom/nasser/naser-group/22_swin0x_vf0xsz.png" alt="Image"></div>
+      <div class="swiper-slide"><img src="https://res.cloudinary.com/adee-anchan/image/upload/v1696580109/valcom/nasser/naser-group/26_hyjdxl_poql2s.png" alt="Image"></div>
+      <div class="swiper-slide"><img src="https://res.cloudinary.com/adee-anchan/image/upload/v1696583090/valcom/nasser/naser-group/clifton1_rpi9sm.webp" alt="Image"></div>
+    </div>
+    <!-- add swiper-wrapper -->
+    <div class="gallery-pagination"></div>
+    <!-- end gallery-pagination -->
+    </div>
+        <h6>Why choose us?</h6>
+        <p class="font-weight-bold">Experience and Reputation</p>
+        <p>Luxury, a word that evokes images of opulence, extravagance, and exclusivity, is a concept that has fascinated and captivated humanity for centuries. It's not merely about possessing expensive items or residing in grand mansions; rather, true luxury encompasses a deeper, more profound essence. At its core, luxury is about the seamless fusion of design, craftsmanship, and impeccable attention to detail.</p><br>
+        <h6>Why choose our project?</h6>
+        <p class="font-weight-bold">Value Engineering</p>
+        <p>In the process of designing our projects, we employ value engineering with a dual focus. Our aim is to create properties that not only yield enhanced capital appreciation for investors but also offer superior value for residents, resulting in a favorable outcome for all parties involved.</p>
+       
+			</div>
+      <!-- <div class="col-lg-12 wow fadeInUp" id="milestones">
+        <h4>Featured Projects
+        </h4>
+          <figure ><a href="https://res.cloudinary.com/adee-anchan/image/upload/v1696597793/valcom/nasser/naser-group/final_ibxeva.webp" data-fancybox> <img src="https://res.cloudinary.com/adee-anchan/image/upload/v1696597793/valcom/nasser/naser-group/final_ibxeva.webp" alt=""> </a></figure>
+          <figure ><a href="https://res.cloudinary.com/adee-anchan/image/upload/v1696597768/valcom/nasser/naser-group/Commercial-Industrial-Milestones_sj4et4.webp" data-fancybox> <img src="https://res.cloudinary.com/adee-anchan/image/upload/v1696597768/valcom/nasser/naser-group/Commercial-Industrial-Milestones_sj4et4.webp" alt=""> </a></figure>
+      </div> -->
+		</div>
+		<!-- end row -->
+	</div>
+	<!-- end container -->
 </section>
+<!-- end about-content -->
 <?php
 include('footer.php');
 ?>
--- index.php
+++ index.php
@@ -1,13 +1,258 @@
 <?php
 include('header.php');
 ?>
-<!-- end page-header -->
-<section >
-	<!-- <img src="" alt=""> -->
-  <div class="coming-soon chutiya">
-    <img src="https://res.cloudinary.com/adee-anchan/image/upload/v1710066610/valcom/27282_oyq7me.jpg" alt="">
+<header class="slider">
+  <div class="slider-container">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide" data-background="https://res.cloudinary.com/adee-anchan/image/upload/v1696580073/valcom/nasser/naser-group/3-min_fr731n_s0wmdq.png" data-stellar-background-ratio="1.15">
+        <div class="container">
+          <h1>Reimagining Excellence</h1>
+          <h5 style="color: #eee;" class="pb-4">We bring innovation and exceptional quality to every construction and development endeavor.</h5>
+        </div>
+        <!-- end container --> 
+      </div>
+      <!-- end swiper-slide -->
+      <div class="swiper-slide" data-background="https://res.cloudinary.com/adee-anchan/image/upload/v1696582999/valcom/nasser/naser-group/clifton3_awwj5j.webp" data-stellar-background-ratio="1.15">
+        <div class="container">
+          <h1>Luxury & Details Fusion</h1>
+          <h5 style="color: #eee;" class="pb-4">Our passion lies in creating luxurious spaces that epitomize sophistication and attention to detail.</h5>
+        </div>
+        <!-- end container --> 
+      </div>
+      <!-- end swiper-slide -->
+      <div class="swiper-slide" data-background="https://res.cloudinary.com/adee-anchan/image/upload/v1696594753/valcom/nasser/naser-group/28_e3yubw.webp" data-stellar-background-ratio="1.15">
+        <div class="container">
+          <h1>Shaping The Future</h1>
+          <h5 style="color: #eee;" class="pb-4">As visionary builders, we are dedicated to shaping the future of construction and development.</h5>
+        </div>
+        <!-- end container --> 
+      </div>
+      <!-- end swiper-slide --> 
+    </div>
+    <!-- Add Pagination -->
+    <div class="inner-elements">
+      <div class="container">
+        <div class="pagination"></div>
+        <!-- end pagination -->
+        <div class="button-prev">PREV</div>
+        <!-- end button-prev -->
+        <div class="button-next">NEXT</div>
+        <!-- end social-media --> 
+      </div>
+      <!-- end container --> 
+    </div>
+    <!-- end inner-elements --> 
+  </div>
+  <!-- end slider-container --> 
+</header>
+<!-- end slider -->
+<section class="intro pb-5">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-6">
+        <figure>
+          <div class="holder" data-stellar-ratio="1.10"> <img src="https://res.cloudinary.com/adee-anchan/image/upload/v1696582990/valcom/nasser/naser-group/image_timsfc.png" alt="Image"></div>
+          <!-- end holder --> 
+        </figure>
+      </div>
+      <!-- end col-6 -->
+      <div class="col-lg-6 wow fadeInUp">
+        <div class="content-box"> <b>01</b>
+          <h4>About <span>Naser</span> </h4>
+          <h3>Redefine the standards of Excellence</h3>
+          <p>For more than two decades, Naser Group has
+            been dedicated to the pursuit of excellence in
+            every project we embark upon.</p>
+          <a href="about-naser.php"> <img src="images/icon-m2.png" alt="Image">Learn More..</a> </div>
+        <!-- end content-box --> 
+      </div>
+      <!-- edn col-6 --> 
+    </div>
+    <!-- end row --> 
+  </div>
+  <!-- end container --> 
+</section>
+<section class="benefits">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 wow fadeInUp"> <b>02</b>
+        <h4><span>Naser</span> in Numbers</h4>
+        <h3>Construction you can count on.</h3>
+      </div>
+      <!-- end col-12 -->
+      <div class="col wow fadeInUp" data-wow-delay="0s">
+        <figure> <img src="https://res.cloudinary.com/adee-anchan/image/upload/v1696580087/valcom/icons/1_yxiram_up7ken.png" alt="Image"> <b></b> </figure>
+        <h6>Project Value</h6>
+        <span class="odometer" data-count="3.24" data-status="yes">0</span> <span class="extra">Billion</span> </div>
+      <!-- end col -->
+      <div class="col wow fadeInUp" data-wow-delay="0.05s">
+        <figure> <img src="https://res.cloudinary.com/adee-anchan/image/upload/v1696580087/valcom/icons/2_yegq8c_nyms1j.png" alt="Image"> <b></b> </figure>
+        <h6>Projects Completed</h6>
+        <span class="odometer" data-count="55" data-status="yes">0</span> <span class="extra"></span> </div>
+      <!-- end col -->
+      <div class="col wow fadeInUp" data-wow-delay="0.10s">
+        <figure> <img src="https://res.cloudinary.com/adee-anchan/image/upload/v1696580087/valcom/icons/2_yegq8c_nyms1j.png" alt="Image"> <b></b> </figure>
+        <h6>Projects Under Development</h6>
+        <span class="odometer" data-count="11" data-status="yes">0</span> <span class="extra"></span> </div>
+      <!-- end col -->
+      <div class="col wow fadeInUp" data-wow-delay="0.15s">
+        <figure> <img src="https://res.cloudinary.com/adee-anchan/image/upload/v1696580088/valcom/icons/4_oesoxn_dopsc0.png" alt="Image"> <b></b> </figure>
+        <h6>in Operation</h6>
+        <span class="odometer" data-count="23" data-status="yes">0</span> <span class="extra">years</span> </div>
+      <!-- end col -->
+    </div>
+    <!-- end row --> 
+  </div>
+  <!-- end container --> 
+</section>
+<!-- end benefits -->
+<section class="recent-gallery">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-5 wow fadeInUp"> <b>03</b>
+        <h4><span>Explore</span> Our Properties</h4>
+        <h3>The creativity of the new world.</h3>
+        <a href="our-communities.php" class="link">SEE ALL GALLERY <i class="fas fa-caret-right"></i></a> </div>
+      <!-- end col-5 -->
+      <div class="col-lg-7">
+        <div class="row inner">
+          <div class="col-md-4 wow fadeInUp" data-wow-delay="0s">
+            <figure data-stellar-ratio="1.07"> <a href="https://res.cloudinary.com/adee-anchan/image/upload/v1696595563/valcom/nasser/naser-group/1-min_klgwrg.png" data-fancybox><img src="https://res.cloudinary.com/adee-anchan/image/upload/v1696595563/valcom/nasser/naser-group/1-min_klgwrg.png" alt="Image"></a> </figure>
+          </div>
+          <!-- end col-4 -->
+          <div class="col-md-4 wow fadeInUp" data-wow-delay="0.05s">
+            <figure data-stellar-ratio="1.15"> <a href="https://res.cloudinary.com/adee-anchan/image/upload/v1696595561/valcom/nasser/naser-group/2-min_j1uaen.png" data-fancybox><img src="https://res.cloudinary.com/adee-anchan/image/upload/v1696595561/valcom/nasser/naser-group/2-min_j1uaen.png" alt="Image"></a> </figure>
+          </div>
+          <!-- end col-4 -->
+          <div class="col-md-4 wow fadeInUp" data-wow-delay="0.10s">
+            <figure data-stellar-ratio="1.04"> <a href="https://res.cloudinary.com/adee-anchan/image/upload/v1696595562/valcom/nasser/naser-group/3-min_ztt1xf.png" data-fancybox><img src="https://res.cloudinary.com/adee-anchan/image/upload/v1696595562/valcom/nasser/naser-group/3-min_ztt1xf.png" alt="Image"></a> </figure>
+          </div>
+          <!-- end col-4 --> 
+        </div>
+        <!-- end row --> 
+      </div>
+      <!-- end col-7 --> 
+    </div>
+    <!-- end row --> 
+  </div>
+  <!-- end container --> 
+</section>
+<!-- end recent-gallery -->
+<section class="property-calculator">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-6">
+        <figure>
+          <div class="holder" data-stellar-ratio="1.07"> <img src="https://res.cloudinary.com/adee-anchan/image/upload/v1696595566/valcom/nasser/naser-group/the_collection_qydcio.png" alt="Image"></div>
+          <!-- end holder --> 
+        </figure>
+      </div>
+      <!-- end col-6 -->
+      <div class="col-lg-6 wow fadeInUp">
+        <div class="content-box"> <b>04</b>
+          <h4><span>Upcoming</span> Project</h4>
+          <h3>The Collection</h3>
+          <p>District 11, Meydan, Dubai, United Arab Emirates</p>
+          <a href="https://www.valcom.ae/the-collection-dubai/">Explore.. </a> </div>
+        <!-- end content-box --> 
+      </div>
+      <!-- end col-6 --> 
+    </div>
+    <!-- end row --> 
+  </div>
+  <!-- end container --> 
+</section>
+
+
+<section class="get-consultation" data-background="https://res.cloudinary.com/adee-anchan/image/upload/v1696594751/valcom/nasser/naser-group/26_srkzr5.webp" data-stellar-background-ratio="0.9">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-6 col-md-8 fadeInUp wow">
+        <div class="content-box"> <b>05</b>
+          <h4><span>What</span> Makes Naser Special</h4>
+          <h3>We Use Exceptional Meterials</h3>
+          <p>Each
+            material converges to shape an architectural masterpiece that
+            seamlessly marries sophistication and comfort.</p>
+          <a href="our-communities.php">PORTFOLIO<i class="fas fa-caret-right"></i></a> </div>
+        <!-- end content-box --> 
+      </div>
+      <!-- end col-6 --> 
+    </div>
+    <!-- end row --> 
+  </div>
+  <!-- end container --> 
+</section>
+
+
+<section class="property-customization">
+  <div class="video-bg">
+    <video src="videos/dubai-video2.mp4" loop autoplay muted></video>
+  </div>
+  <!-- end video-bg -->
+  <div class="container">
+    <div class="row video-background">
+      <div class="col-12 wow fadeInUp"> <b></b>
+        <h4 style="font-size: 55px;"><span>Naser</span> Vision </h4>
+        <h3>"Building Dreams, Crafting Destinies - Your Dubai Oasis Awaits!"</h3>
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0s">
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.05s">
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.10s">
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.15s">
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.20s">
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.25s">
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0s">
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.05s">
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.10s">
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.15s">
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.20s">
+      </div>
+      <div class="col-lg-2 col-md-4 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.25s">
+      </div>
+    </div>
   </div>
 </section>
+<!-- end property-customization -->
+<section class="certificates">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-lg-4 col-md-12 wow fadeInUp"> <b>06</b>
+        <h4><span>Under</span> Construction</h4>
+        <small> </small> </div>
+      <!-- end col-5 -->
+      <div class="col-lg-2 col-md-3 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.05s">
+        <figure> <a href="https://res.cloudinary.com/adee-anchan/image/upload/v1697462987/valcom/nasser/naser-group/building/2021-Ivanhoe-GardensStg2-Thumbnail_fjbfqk.webp" data-fancybox><img src="https://res.cloudinary.com/adee-anchan/image/upload/v1697462987/valcom/nasser/naser-group/building/2021-Ivanhoe-GardensStg2-Thumbnail_fjbfqk.webp" alt="Image"></a> </figure>
+      </div>
+      <!-- end col-3 -->
+      <div class="col-lg-2 col-md-3 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.10s">
+        <figure> <a href="https://res.cloudinary.com/adee-anchan/image/upload/v1697462988/valcom/nasser/naser-group/building/tile-5_2x_afdbhk.webp" data-fancybox><img src="https://res.cloudinary.com/adee-anchan/image/upload/v1697462988/valcom/nasser/naser-group/building/tile-5_2x_afdbhk.webp" alt="Image"></a> </figure>
+      </div>
+      <!-- end col-3 -->
+      <div class="col-lg-2 col-md-3 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.15s">
+        <figure> <a href="https://res.cloudinary.com/adee-anchan/image/upload/v1697462987/valcom/nasser/naser-group/building/2022-Porter-Thumbnail_fu2vwj.webp" data-fancybox><img src="https://res.cloudinary.com/adee-anchan/image/upload/v1697462987/valcom/nasser/naser-group/building/2022-Porter-Thumbnail_fu2vwj.webp" alt="Image"></a> </figure>
+      </div>
+      <!-- end col-3 -->
+      <div class="col-lg-2 col-md-3 col-sm-6 col-6 wow fadeInUp" data-wow-delay="0.20s">
+        <figure> <a href="https://res.cloudinary.com/adee-anchan/image/upload/v1697462987/valcom/nasser/naser-group/building/2023-SignalHouseThumbnail_d8uxgk.webp" data-fancybox><img src="https://res.cloudinary.com/adee-anchan/image/upload/v1697462987/valcom/nasser/naser-group/building/2023-SignalHouseThumbnail_d8uxgk.webp" alt="Image"></a> </figure>
+      </div>
+      <!-- end col-3 --> 
+    </div>
+    <!-- end row --> 
+  </div>
+  <!-- end container --> 
+</section>
+<!-- end certificates -->
 <?php
 include('footer.php');
 ?>
