8c6d26896bbf05743f3ff25feaa88c6188927641
digitizercorp <digitizercorpuae@gmail.com>
2024-03-25 20:49:50 +0400

    header-footer

--- footer.php
+++ footer.php
@@ -1,4 +1,70 @@
-
+<section class="footer-bar">
+  <div class="container">
+    <div class="inner wow fadeIn">
+      <div class="row">
+        <div class="col-lg-4 wow fadeInUp" data-wow-delay="0.05s">
+          <figure><img src="images/footer-icon01.png" alt="Image"></figure>
+          <h3>Address</h3>
+          <p>Level 1, 109 Drummond Street,<br>
+          Carlton Vic 3053
+          </p>
+        </div>
+        <!-- end col-4 -->
+        <div class="col-lg-4 wow fadeInUp" data-wow-delay="0.10s">
+          <figure><img src="images/footer-icon02.png" alt="Image"></figure>
+          <h3>Working Hours</h3>
+          <p>Monday to Friday <strong>09:00</strong> to <strong>18:30</strong> <br>
+            Saturday to Sunday <strong>10:00</strong> to <strong>17:30</strong></p>
+        </div>
+        <!-- end col-4 -->
+        <div class="col-lg-4 wow fadeInUp" data-wow-delay="0.15s">
+          <figure><img src="images/footer-icon03.png" alt="Image"></figure>
+          <h3>Sales Office</h3>
+          <p>Valcom Properties, O 14 Tower, Marasi Drive, Business Bay, Dubai, United Arab Emirates</p>
+        </div>
+        <!-- end col-4 --> 
+      </div>
+      <!-- end row --> 
+    </div>
+    <!-- end inner --> 
+  </div>
+  <!-- end container --> 
+</section>
+<!-- end footer-bar -->
+<footer class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 wow fadeInUp" data-wow-delay="0.05s"> <img src="https://res.cloudinary.com/adee-anchan/image/upload/v1696580072/valcom/nasser/logo/naser_logo_white_transparent_ysxpa5_mcue2h.png" alt="Image" class="logo">
+        <p>We are construction and development company specializing in the construction and interior fit out of residential, commercial and industrial projects.</p>
+      </div>
+      <!-- end col-4 -->
+      <div class="col-lg-2 col-md-6 wow fadeInUp" data-wow-delay="0.10s">
+        <ul class="footer-menu">
+          <li><a href="index.php">Home</a></li>
+          <li><a href="about-naser.php">About us</a></li>
+          <li><a href="our-communities.php">Our Communities</a></li>
+          <li><a href="about-naser.php#milestones">Milestone</a></li>
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+      </div>
+      <!-- end col-2 -->
+      <div class="col-lg-4 wow fadeInUp" data-wow-delay="0.20s">
+        <div class="contact-box">
+          <h5>CALL CENTER</h5>
+          <h3>+971 4 492 7000</h3>
+          <p><a href="#">info@valcom.ae</a></p>
+        </div>
+        <!-- end contact-box --> 
+      </div>
+      <!-- end col-4 -->
+      <div class="col-12"> <span class="copyright">© 2023 Nasergroup | Valcom Properties</span> </div>
+      <!-- end col-12 --> 
+    </div>
+    <!-- end row --> 
+  </div>
+  <!-- end container --> 
+</footer>
+<!-- end footer --> 
 
 <!-- JS FILES --> 
 <script src="js/jquery.min.js"></script> 
